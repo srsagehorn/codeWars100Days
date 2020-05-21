c2ecf920aa8ffcb3f1b999a60b24e36598b4dcf0
Shannon Sagehorn <srsagehorn@gmail.com>
2020-05-20 22:13:57 -0500

    complete

---
+++ 1-10/day5.js
@@ -0,0 +1,14 @@
+// 8 kyu
+// Get ASCII value of a character
+
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+function getASCII(c) {
+  return c.charCodeAt();
+}
+
+// Test.assertEquals(getASCII('A'),65)
+// Test.assertEquals(getASCII(' '),32)
+// Test.assertEquals(getASCII('!'),33)
